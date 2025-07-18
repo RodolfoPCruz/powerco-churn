0a7622576523759334eab5c342bf9a227de5d6f8
Rodolfo <rodolfo_pinheiro@hotmail.com>
2025-07-18 15:53:16 -0300

    feat:  correcoes na file train_model.py. Esta funcionando com a pipeline de preprocessamento separada do modelo de ml.

--- src/powerco_churn/train_model.py
+++ src/powerco_churn/train_model.py
@@ -7,17 +7,19 @@ from sklearn.model_selection import cross_validate
 from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score
 import argparse
 from powerco_churn.utils.log_mlflow_utils import load_logged_dataset
+from sklearn.pipeline import Pipeline
 
 current_file = Path(__file__).resolve()
 base_path = current_file.parents[2]  # 0 is file, 1 is parent, ..., 3 = three levels up
 base_path = str(base_path)
 
+#run id generated when the raw dataset was logged
 run_id_raw_dataset = '8cd45ac031db4540b94bdc7efd917411'
 
 mlflow.set_tracking_uri(f"file:{base_path}/mlruns")  # Goes one level up
 mlflow.set_experiment("powerco_churn")
 
-
+#loading raw dataset
 client_data_train = load_logged_dataset(run_id_raw_dataset, 'datasets/client_train_data/client_train_data.parquet')
 client_data_test = load_logged_dataset(run_id_raw_dataset, 'datasets/client_test_data/client_test_data.parquet')
 
@@ -47,13 +49,15 @@ scoring = {
     'recall': 'recall'
 }
 
+#run id generated when the pre-processing pipeline was logged
 run_id_pipeline = '8142c0c2ca004887a918401cf94bf952'
 pre_process_pipeline = mlflow.sklearn.load_model(f"runs:/{run_id_pipeline}/preprocessing_pipeline")
 
+#preprocessing the raw data
 x_train = pre_process_pipeline.fit_transform([client_data_train, price_data_train])
 x_test = pre_process_pipeline.transform([client_data_test, price_data_test])
 
-
+#training the model using cross validation
 results = cross_validate(model_lgbm, 
                             x_train, 
                             y_train,
@@ -61,29 +65,37 @@ results = cross_validate(model_lgbm,
                         scoring=scoring)
 results_df = pd.DataFrame(results)
 results_df.to_csv('src/powerco_churn/artifacts/cross_val_metrics.csv')
-mlflow.log_artifact("src/powerco_churn/artifacts/cross_val_metrics.csv")
+#mlflow.log_artifact("src/powerco_churn/artifacts/cross_val_metrics.csv")
 
-print("Accuracy: ", results['test_accuracy'].mean())
-print("Accuracy per fold:", results['test_accuracy'])
-print("Precision per fold:", results['test_precision'])
-print("Recall per fold:", results['test_recall'])
-
-print(len(x_train))
-print(len(x_test))
-print(len(y_train))
-print(len(y_test))
-
-
+#training the model using the enrire dataset. The model will be evaluated
+#using the test data
 model_lgbm.fit(x_train, y_train)
 y_pred = model_lgbm.predict(x_test)
     
 acc_test = accuracy_score(y_test, y_pred)
-mlflow.log_metric("test_accuracy", acc_test)
+#mlflow.log_metric("test_accuracy", acc_test)
 
 precision_test = precision_score(y_test, y_pred)
-mlflow.log_metric("test_precision", precision_test)
+#mlflow.log_metric("test_precision", precision_test)
 
 recall_test = recall_score(y_test, y_pred)
-mlflow.log_metric("test_recall", recall_test)
+#mlflow.log_metric("test_recall", recall_test)
 
-mlflow.sklearn.log_model(model_lgbm, "model")
+
+#Creating a new pipeline that integrates the preprocessing step and the model
+
+
+
+
+
+#mlflow.sklearn.log_model(model_lgbm, "model")
+with mlflow.start_run(run_name = "train_model"):
+    mlflow.log_param("n_estimators", args.n_estimators)
+    mlflow.log_param("scale_pos_weight", args.scale_pos_weight)
+    mlflow.log_param("random_state", args.random_state)
+    mlflow.log_param("class_weight", args.class_weight)
+    mlflow.log_artifact("src/powerco_churn/artifacts/cross_val_metrics.csv")
+    mlflow.log_metric("test_accuracy", acc_test)
+    mlflow.log_metric("test_precision", precision_test)
+    mlflow.log_metric("test_recall", recall_test)
+    mlflow.sklearn.log_model(model_lgbm, "model")
